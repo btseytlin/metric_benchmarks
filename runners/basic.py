903cd10a598da0264b906a4f06c7ef5981829423
Boris <b.tseytlin@lambda-it.ru>
2020-09-01 17:11:06 +0300

    initial

---
+++ runners/basic.py
@@ -0,0 +1,4 @@
+from base import get_runner
+
+r = get_runner()
+r.run()
---
+++ runners/triplet_contrastive_minterm.py
@@ -0,0 +1,91 @@
+from base import get_runner
+
+from pytorch_metric_learning.losses.generic_pair_loss import GenericPairLoss
+from pytorch_metric_learning.losses.base_metric_loss_function import BaseMetricLossFunction
+from pytorch_metric_learning.utils import loss_and_miner_utils as lmu
+from pytorch_metric_learning.reducers import AvgNonZeroReducer
+import torch.nn.functional as F
+import torch
+
+from pytorch_metric_learning.losses.generic_pair_loss import GenericPairLoss
+from pytorch_metric_learning.losses.base_metric_loss_function import BaseMetricLossFunction
+from pytorch_metric_learning.utils import loss_and_miner_utils as lmu
+from pytorch_metric_learning.reducers import AvgNonZeroReducer
+
+from pytorch_metric_learning.losses.generic_pair_loss import GenericPairLoss
+from pytorch_metric_learning.losses.base_metric_loss_function import BaseMetricLossFunction
+from pytorch_metric_learning.utils import loss_and_miner_utils as lmu
+from pytorch_metric_learning.reducers import AvgNonZeroReducer
+
+# Warning: works only with distances, not similarities
+class TripletContrastiveLossMinterm(BaseMetricLossFunction):
+    def __init__(
+        self,
+        pos_margin=0.5,
+        neg_margin=0.5,
+        triplet_margin=0.1,
+        triplets_per_anchor="all",
+        distance_norm=2,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.pos_margin = pos_margin
+        self.neg_margin = neg_margin
+        self.triplet_margin = triplet_margin
+        self.triplets_per_anchor = triplets_per_anchor
+        self.distance_norm = distance_norm
+        
+        self.add_to_recordable_attributes(list_of_names=["num_non_zero_pos_pairs", "num_non_zero_neg_pairs"])
+        self.add_to_recordable_attributes(name="num_non_zero_triplets_triplet_loss_only")
+        self.add_to_recordable_attributes(name="num_non_zero_triplets")
+    
+    def compute_loss(self, embeddings, labels, indices_tuple):
+        self.num_non_zero_pos_pairs, self.num_non_zero_neg_pairs = 0, 0
+        self.num_non_zero_triplets_triplet_loss_only = 0
+        self.num_non_zero_triplets = 0
+            
+        indices_tuple = lmu.convert_to_triplets(indices_tuple, labels, t_per_anchor=self.triplets_per_anchor)
+        
+        anchor_idx, positive_idx, negative_idx = indices_tuple
+        if len(anchor_idx) == 0:
+            return self.zero_losses()
+        
+        anchors, positives, negatives = embeddings[anchor_idx], embeddings[positive_idx], embeddings[negative_idx]
+
+        a_p_dist = F.pairwise_distance(anchors, positives, self.distance_norm)
+        a_n_dist = F.pairwise_distance(anchors, negatives, self.distance_norm)
+        p_n_dist = F.pairwise_distance(positives, negatives, self.distance_norm)
+        
+        #print('AP', a_p_dist, 'AN', a_n_dist, 'PN', p_n_dist)
+        dist = a_p_dist - torch.min(a_n_dist, p_n_dist)
+        
+        # Compute triplet loss
+        triplet_loss = F.relu(dist + self.triplet_margin)
+        self.num_non_zero_triplets_triplet_loss_only = (triplet_loss > 0).nonzero().size(0)
+        #print('Triplet loss', triplet_loss)
+        
+        # Compute pos contrastive loss
+        contrastive_pos = F.relu(a_p_dist - self.pos_margin)
+        self.num_non_zero_pos_pairs = (contrastive_pos > 0).nonzero().size(0)
+        #print('Contrastive pos', contrastive_pos)
+        
+        # Compute neg contrastive loss
+        contrastive_neg = F.relu(self.neg_margin - torch.min(a_n_dist, p_n_dist))
+        self.num_non_zero_neg_pairs = (contrastive_neg > 0).nonzero().size(0)
+        #print('Contrastive neg', contrastive_neg)
+        
+        full_loss = triplet_loss + contrastive_pos + contrastive_neg 
+        self.num_non_zero_triplets = (full_loss > 0).nonzero().size(0)
+        
+        #print(full_loss)
+        loss_dict = {"loss": {"losses": full_loss, "indices": anchor_idx, "reduction_type": "element"}}
+        return loss_dict
+        
+    def get_default_reducer(self):
+        return AvgNonZeroReducer()
+
+r = get_runner()
+
+r.register("loss", TripletContrastiveLossMinterm)
+
+r.run()
