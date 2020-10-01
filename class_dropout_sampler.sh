eb6c2d932e0fe321c0e0f877ee51156294582147
Boris <b.tseytlin@lambda-it.ru>
2020-10-01 15:59:49 +0300

    remove custom hook factories from runners where they are not needed

--- class_dropout_sampler.sh
+++ class_dropout_sampler.sh
@@ -3,7 +3,6 @@ rm -r $PWD/experiments/class_dropout_sampler
 python3 runners/class_dropout_sampler.py --experiment_name class_dropout_sampler \
 --dataset~OVERRIDE~ {CUB200: {download: True}} \
 --bayes_opt_iters 25 \
---factories {hook~OVERRIDE~: {CustomHookFactory: {}}} \
 --sampler~OVERRIDE~ {ClassDropoutSampler: {m: 4, d~BAYESIAN~: [0, 1]}} \
 --loss_funcs~OVERRIDE~ \
 {metric_loss: {ContrastiveLoss: {\
