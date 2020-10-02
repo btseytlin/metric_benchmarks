e1aa807cdea863daff5b112cae70f86516f91174
Boris <b.tseytlin@lambda-it.ru>
2020-10-02 13:01:06 +0300

    FOR REAL

--- weighted_sampler_reproduce.sh
+++ weighted_sampler_reproduce.sh
@@ -1,6 +1,7 @@
 #rm -r $PWD/experiments/weighted_sampler
 
 python3 runners/weighted_sampler.py --experiment_name weighted_sampler_reproduce \
+--bayes_opt_iters 0 \
 --dataset~OVERRIDE~ {CUB200: {download: True}} \
 --factories {hook~OVERRIDE~: {CustomHookFactory: {}}} \
 --sampler~OVERRIDE~ {ClassWeightedSampler: {m: 6, mode: 'scores'}} \
