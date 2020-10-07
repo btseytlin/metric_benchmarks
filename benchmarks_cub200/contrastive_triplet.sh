2838ab1a35952ca2f15142c2f0ff8c6b3e41b8f7
Boris <b.tseytlin@lambda-it.ru>
2020-10-07 12:19:53 +0300

    fix bayesian opts

--- benchmarks_cub200/contrastive_triplet.sh
+++ benchmarks_cub200/contrastive_triplet.sh
@@ -1,4 +1,5 @@
 python3 runners/contrastive_triplet.py --experiment_name contrastive_triplet \
+--bayes_opt_iters 25 \
 --dataset~OVERRIDE~ {CUB200: {download: True}} \
 --loss_funcs~OVERRIDE~ \
 {metric_loss: {ContrastiveTripletLoss: {\
