241ebad2ddc679001f0263a9cf5667a50799818b
Boris <b.tseytlin@lambda-it.ru>
2020-10-16 12:59:11 +0300

    Remove outdated runners, fix global er

--- runners/global_embedding_regularizer.py
+++ runners/global_embedding_regularizer.py
@@ -31,11 +31,23 @@ def global_centroid_regularizer(embeddings,
                                 reg_warmup_current=0):
     if not centroids:
         return torch.zeros((len(embeddings), )), torch.zeros((len(embeddings), ))
-    uniq_labels = np.array(torch.unique(labels).cpu())
+    uniq_labels = sorted(np.array(torch.unique(labels).cpu()))
+
+    print('Unique labels according to batch', uniq_labels)
 
     label_to_centroid_idx = {label: idx for idx, label in enumerate(centroids.keys())}
     centroid_vectors = torch.stack(list(centroids.values()))
     
+    print('labels to centroids', label_to_centroid_idx)
+
+    """
+    labels to centroids {25: 0, 26: 1, 27: 2, 28: 3, 29: 4, 30: 5, 31: 6, 32: 7, 33: 8, 34: 9, 35: 10, 36: 11, 37: 12, 38: 13, 39: 14, 40: 15, 41: 16, 42: 17, 43: 18, 44: 19, 45: 20, 46: 21, 47: 22, 48: 23, 49: 24, 50: 25, 51: 26, 52: 27, 53: 28, 54: 29, 55: 30, 56: 31, 57: 32, 58: 33, 59: 34, 60: 35, 61: 36, 62: 37, 63: 38, 64: 39, 65: 40, 66: 41, 67: 42, 68: 43, 69: 44, 70: 45, 71: 46, 72: 47, 73: 48, 74: 49, 75: 50, 76: 51, 77: 52, 78: 53, 79: 54, 80: 55, 81: 56, 82: 57, 83: 58, 84: 59, 85: 60, 86: 61, 87: 62, 88: 63, 89: 64, 90: 65, 91: 66, 92: 67, 93: 68, 94: 69, 95: 70, 96: 71, 97: 72, 98: 73, 99: 74}
+    
+    labels to centroids {25: 0, 26: 1, 27: 2, 28: 3, 29: 4, 30: 5, 31: 6, 32: 7, 33: 8, 34: 9, 35: 10, 36: 11, 37: 12, 38: 13, 39: 14, 40: 15, 41: 16, 42: 17, 43: 18, 44: 19, 45: 20, 46: 21, 47: 22, 48: 23, 49: 24, 50: 25, 51: 26, 52: 27, 53: 28, 54: 29, 55: 30, 56: 31, 57: 32, 58: 33, 59: 34, 60: 35, 61: 36, 62: 37, 63: 38, 64: 39, 65: 40, 66: 41, 67: 42, 68: 43, 69: 44, 70: 45, 71: 46, 72: 47, 73: 48, 74: 49, 75: 50, 76: 51, 77: 52, 78: 53, 79: 54, 80: 55, 81: 56, 82: 57, 83: 58, 84: 59, 85: 60, 86: 61, 87: 62, 88: 63, 89: 64, 90: 65, 91: 66, 92: 67, 93: 68, 94: 69, 95: 70, 96: 71, 97: 72, 98: 73, 99: 74}
+
+
+    """
+
     if embeddings.get_device() > -1:
         centroid_vectors = centroid_vectors.to(embeddings.get_device())
     
@@ -49,10 +61,12 @@ def global_centroid_regularizer(embeddings,
                                  squared=False)
     
     warmup_coef = np.clip(np.log(reg_warmup_current)/np.log(reg_warmup), 0, 1)
+
+    print('warmup', warmup_coef)
     for i, label in enumerate(uniq_labels):
         #print(label)
         label_indices = torch.where(labels.flatten()==label)[0]
-        centroid_idx = label_to_centroid_idx[label]
+        centroid_idx = label_to_centroid_idx[int(label)]
         
         dist_to_own_centroid = dists[label_indices, centroid_idx]
         #print('To own centroid', dist_to_own_centroid)
@@ -115,10 +129,12 @@ class ContrastiveLossRegularized(ContrastiveLoss):
                     "reduction_type": "element"}
         loss_dict['reg_own_centroid'] = reg_dict
 
+
         reg_dict = {"losses": reg_other_centroids,
                     "indices": torch.tensor(list(range(len(embeddings)))),
                     "reduction_type": "element"}
         loss_dict['reg_other_centroids'] = reg_dict
+
         return loss_dict
     
     def get_default_reducer(self):
@@ -150,19 +166,29 @@ class CustomHookFactory(HookFactory):
 
         def get_centroids(dataset, models):
             embeddings, labels = tester.get_all_embeddings(dataset, models['trunk'], models['embedder'], collate_fn)
-            uniq_labels = np.unique(labels.flatten())
+            print(labels[:10])
+            uniq_labels = sorted(np.unique(labels.flatten()))
+            print('Unique labels according to hook factory', uniq_labels)
             centroids = {}            
             for label in uniq_labels:
-                label_indices = np.where(labels.flatten()==label)[0]
+                label_indices = np.where(labels.flatten() == label)[0]
                 label_embeddings = embeddings[label_indices, :]
-                centroids[label] = torch.Tensor(np.mean(label_embeddings, axis=0))
+                centroids[int(label)] = torch.Tensor(np.mean(label_embeddings, axis=0))
             return centroids
         
         def end_of_epoch_hook(trainer):
             models = trainer.models
+            train_dataset = split_manager.get_dataset("eval", "train")
+
+            train_label_set = split_manager.get_label_set("eval", "train")
+            print('Train label set', train_label_set)
+            print('Test label set', split_manager.get_label_set("eval", "test"))
+            print('Val label set', split_manager.get_label_set("eval", "val"))
             centroids = OrderedDict(get_centroids(train_dataset, models))
+            print('Obtained centroids')
             trainer.loss_funcs['metric_loss'].centroids = centroids
             trainer.loss_funcs['metric_loss']._warmup = trainer.epoch
+
             return helper_hook(trainer)
 
         return end_of_epoch_hook
