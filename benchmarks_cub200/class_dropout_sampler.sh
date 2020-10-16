f84e4b82da4a9f962e7fae88f7d831993b088b44
Boris <b.tseytlin@lambda-it.ru>
2020-10-16 12:57:28 +0300

    Fix class dropout sampler sh

--- benchmarks_cub200/class_dropout_sampler.sh
+++ benchmarks_cub200/class_dropout_sampler.sh
@@ -3,6 +3,7 @@
 python3 runners/class_dropout_sampler.py --experiment_name class_dropout_sampler \
 --dataset~OVERRIDE~ {CUB200: {download: True}} \
 --bayes_opt_iters 25 \
+--factories {hook~OVERRIDE~: {CustomHookFactory: {}}} \
 --sampler~OVERRIDE~ {ClassDropoutSampler: {m: 4, d~BAYESIAN~: [0, 1]}} \
 --loss_funcs~OVERRIDE~ \
 {metric_loss: {ContrastiveLoss: {\
--- benchmarks_cub200/global_embedding_regularizer.sh
+++ benchmarks_cub200/global_embedding_regularizer.sh
@@ -1,6 +1,7 @@
 python3 runners/global_embedding_regularizer.py --experiment_name global_embedding_regularizer \
 --dataset~OVERRIDE~ {CUB200: {download: True}} \
 --bayes_opt_iters 25 \
+--factories {hook~OVERRIDE~: {CustomHookFactory: {}}} \
 --loss_funcs~OVERRIDE~ \
 {metric_loss: {ContrastiveLossRegularized: {\
 reg_own_weight~BAYESIAN~: [0, 1], reg_own_threshold~BAYESIAN~: [0, 1], \
