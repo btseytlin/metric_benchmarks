16fea3ee6a02d00d25594ce9baa7bbb01b06065a
Boris <b.tseytlin@lambda-it.ru>
2020-10-26 14:02:15 +0300

    Add hotels50k subset

--- runners/hotels50k.py
+++ runners/hotels50k.py
@@ -1,25 +1,17 @@
 import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
 import numpy as np
-
+import pandas as pd
 import csv, multiprocessing, cv2, os
-import numpy as np
 import urllib
 import urllib.request
 import subprocess
-
 from tqdm import tqdm
-
 from powerful_benchmarker.split_managers import ClassDisjointSplitManager, BaseSplitManager, IndexSplitManager
-from torchvision import transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, Dataset
-import torch
-import numpy as np
 import pickle
 import lmdb
-
 from utils import ImageFolderLMDB, folder2lmdb, raw_reader, compress_serialize
 
 
@@ -76,16 +68,6 @@ def make_lmdb(root, out_path, write_frequency=5000, num_workers=8, map_size=1e11
                 for data in data_loader:
                     image, im_source = data[0]
 
-                # for im_source in os.listdir(os.path.join(root, 'images', dataset_dir, chain, hotel)):
-                #     if im_source.startswith('.'):
-                #         continue
-                #     for image_name in os.listdir(os.path.join(root, 'images', dataset_dir, chain, hotel, im_source)):
-                #         if image_name.startswith('.'):
-                #             continue
-                #         img_path = os.path.join(root, 'images', dataset_dir, chain, hotel, im_source, image_name)
-
-                #         with open(img_path, 'rb') as f:
-                #             image = f.read()
 
                     txn.put(u'{}'.format(idx).encode('ascii'), compress_serialize((image, chain, hotel, im_source)))
 
@@ -120,11 +102,11 @@ def make_lmdb(root, out_path, write_frequency=5000, num_workers=8, map_size=1e11
             pickle.dump(hotels, f)
 
 
-def download_and_resize(root, image_list):
+def download_and_resize(root, image_list, basedir='images/train'):
     for im in image_list:
         try:
-            (chain,hotel,im_source,im_id,im_url) = im
-            saveDir = os.path.join(root, 'images/train/', chain, hotel, im_source)
+            (chain,hotel,im_source,im_id,im_url) = tuple([str(x) for x in im])
+            saveDir = os.path.join(root, basedir, chain, hotel, im_source)
             if not os.path.exists(saveDir):
                 os.makedirs(saveDir)
 
@@ -146,7 +128,8 @@ def download_and_resize(root, image_list):
                 print('Saved: ' + savePath)
             else:
                 print('Already saved: ' + savePath)
-        except Exception:
+        except Exception as e:
+            print(e)
             print('Failed to download image')
 
 def download_train_images(root):
@@ -182,10 +165,94 @@ def download_train_images(root):
     pool.join()
     print('Done downloading train')
 
+def download_images_subset(root, df, basedir):
+    images = []
+    for ix, row in df.iterrows():
+        im_id = row.image_id
+        im_url = row.image_url
+        im_source = row.image_source
+        hotel = row.hotel_id
+        chain = row.chain_id
+        images.append((chain,hotel,im_source,im_id,im_url))
+
+    NUM_THREADS = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(NUM_THREADS)
+
+    imDict = {}
+    for cpu in range(NUM_THREADS):
+        pool.apply_async(download_and_resize, [root, images[cpu::NUM_THREADS]], {'basedir': basedir})
+    print('Started subprocesses')
+    pool.close()
+    print('Joining threads')
+    pool.join()
+    print('Done downloading train')
 
 
+def prepare_hotels50k_subset(root, seed=0):
+    np.random.seed(seed)
+    hinfo = pd.read_csv(os.path.join(root, 'input', 'dataset',  'hotel_info.csv'))
+    
+    hotels_to_chain = hinfo[['hotel_id', 'chain_id']]
+    hotels_to_chain.index = hotels_to_chain['hotel_id']
+    hotels_to_chain = hotels_to_chain['chain_id']
+    
+    df = pd.read_csv(os.path.join(root, 'input', 'dataset', 'train_set.csv'), header=None)
+    df.columns = ['image_id', 'hotel_id', 'image_url', 'image_source', 'upload_timestamp']
+    df['chain_id'] = hotels_to_chain[df.hotel_id].values
+    
+    photos_per_hotel = df.groupby(['hotel_id']).agg({'image_url': 'count'})
+    
+    photos_per_chain = df.groupby(['chain_id']).agg({'image_url': 'count'})
+    
+    # Remove chains with <= 100 photos
+    bad_chains = set(photos_per_chain[(photos_per_chain.reset_index()['image_url'] < 100).values].index)
+    
+    bad_chain_hotels = []
+    for chain in bad_chains:
+        chain_hotels = hinfo[['hotel_id', 'chain_id']][hinfo['chain_id'] == chain]
+        bad_chain_hotels += list(chain_hotels['hotel_id'])
+    
+    # Remove hotels with <= 10 photos
+    bad_hotels = list(photos_per_hotel[(photos_per_hotel.reset_index()['image_url'] < 10).values].index)
+    
+    bad_hotels += bad_chain_hotels
+    
+    df = df[~df.hotel_id.isin(bad_hotels)]
+    
+    # Keep all trafficcam images, sample the rest
+    trafficcam_df = df[df.image_source == 'traffickcam']
+    other_df = df[df.image_source != 'traffickcam']
+    
+    sample_size = len(trafficcam_df)
+    other_df = other_df.sample(sample_size)
+    
+    subset_train_df = pd.concat([trafficcam_df, other_df])
+    
+    
+    test_df = pd.read_csv(os.path.join(root, 'input', 'dataset', 'test_set.csv'))
+    test_df['chain_id'] = hotels_to_chain[test_df.hotel_id].values
+    
+    subset_test_df = test_df[test_df.hotel_id.isin(subset_train_df.hotel_id)]
+    
+    return subset_train_df, subset_test_df
 
-def download_hotels_50k(root):
+def print_subset_stats(subset_train_df, subset_test_df):
+    print('Prepared dataset subset')
+    print('Train chains:', subset_train_df.chain_id.value_counts().shape[0])
+    print('Test chains:', subset_test_df.chain_id.value_counts().shape[0])
+    print('Train hotels:', subset_train_df.hotel_id.value_counts().shape[0])
+    print('Test hotels:', subset_test_df.hotel_id.value_counts().shape[0])
+
+    print('Train images', subset_train_df.shape[0])
+    print('Train traffickcam images', subset_train_df[subset_train_df.image_source == 'traffickcam'].shape[0])
+    
+    print('Test images', subset_test_df.shape[0])
+    print('Test traffickcam images', subset_test_df[subset_test_df.image_source == 'traffickcam'].shape[0])
+
+    return subset_train_df, subset_test_df
+
+
+def download_hotels_50k(root, subset=True, train_subset_path=None, test_subset_path=None):
     print(f'Cloning https://github.com/GWUvision/Hotels-50K into {root}')    
     if not os.path.exists(os.path.join(root, 'input')):
         subprocess.run(['git', 'clone', 'https://github.com/GWUvision/Hotels-50K.git', root],  capture_output=True, check=True)
@@ -200,27 +267,57 @@ def download_hotels_50k(root):
     else:
         print('Archive already unpacked')
 
-    print('Downloading train images')
-    if os.path.exists(os.path.join(root, 'images', 'train')):
-        print('Train images already loaded')
-    else:
-        download_train_images(root)
+    train_csv = os.path.join(root, 'input', 'dataset', 'train_set.csv')
+    test_csv = os.path.join(root, 'input', 'dataset', 'test_set.csv')
+    if subset:
+        print('Loading dataset subset')
+        train_subset_path = train_subset_path or os.path.join(root, 'input', 'train_subset.csv')
+        test_subset_path = test_subset_path or os.path.join(root, 'input', 'test_subset.csv')
+        if not os.path.exists(train_subset_path) or not os.path.exists(test_subset_path):
+            train_df, test_df = prepare_hotels50k_subset(root)
 
-    print('Downloading test images archive')
-    url = 'https://cs.slu.edu/~stylianou/images/hotels-50k/test.tar.lz4'
-    archive_path = os.path.join(root, 'images', 'test.tar.lz4')
-    if not os.path.exists(archive_path):
-        subprocess.run(['wget', url, '-O', archive_path, '--no-check-certificate'],  capture_output=True, check=True)
-    else:
-        print('Archive already downloaded')
+            train_df.to_csv(train_subset_path, index=None)
+            test_df.to_csv(test_subset_path, index=None)
+        else:
+            print('Subset csvs already exist')
+            train_df = pd.read_csv(train_subset_path)
+            test_df = pd.read_csv(test_subset_path)
 
-    out_path = os.path.join(root, 'images')
-    if not os.path.exists(os.path.join(out_path, 'test')):
-        command = ['lz4', '-dc', '--no-sparse',  archive_path, '|',  'tar', '-C', out_path, '-xz']
-        print(' '.join(command))
-        subprocess.run(command,  capture_output=True, check=True)
+
+        print('Loading subset csvs')
+        train_csv = os.path.join(root, 'input', 'train_subset.csv')
+        test_csv = os.path.join(root, 'input', 'test_subset.csv')
+
+        print_subset_stats(train_df, test_df)
+
+        print('Downloading train subset images')
+        download_images_subset(root, train_df, basedir='images/train')
+
+        print('Downloading test subset images')
+        download_images_subset(root, test_df, basedir='images/test/unoccluded')
+
     else:
-        print('Archive already unpacked')
+        print('Downloading train images')
+        if os.path.exists(os.path.join(root, 'images', 'train')):
+            print('Train images already loaded')
+        else:
+            download_train_images(root)
+
+        print('Downloading test images archive')
+        url = 'https://cs.slu.edu/~stylianou/images/hotels-50k/test.tar.lz4'
+        archive_path = os.path.join(root, 'images', 'test.tar.lz4')
+        if not os.path.exists(archive_path):
+            subprocess.run(['wget', url, '-O', archive_path, '--no-check-certificate'],  capture_output=True, check=True)
+        else:
+            print('Archive already downloaded')
+
+        out_path = os.path.join(root, 'images')
+        if not os.path.exists(os.path.join(out_path, 'test')):
+            command = ['lz4', '-dc', '--no-sparse',  archive_path, '|',  'tar', '-C', out_path, '-xz']
+            print(' '.join(command))
+            subprocess.run(command,  capture_output=True, check=True)
+        else:
+            print('Archive already unpacked')
 
     print('Creaing LMDB files')
     lmdb_path = os.path.join(root, 'lmdb')
@@ -236,11 +333,15 @@ def download_hotels_50k(root):
 
 
 class Hotels50kDataset(Dataset):
-    def __init__(self, root, target='chains', transform=None, download=False):
+    def __init__(self, root, target='chains', transform=None, download=False, subset=True, train_subset_path=None, test_subset_path=None):
         assert target in ('chains', 'hotels')
         self.target = target
+
+
         if download:
-            download_hotels_50k(root)
+            train_subset_path = train_subset_path or os.environ.get('HOTELS50K_TRAIN_SUBSET')
+            test_subset_path = test_subset_path or os.environ.get('HOTELS50K_TEST_SUBSET')
+            download_hotels_50k(root, subset=subset, train_subset_path=train_subset_path, test_subset_path=test_subset_path)
 
         train_path = os.path.join(root, 'lmdb', 'train.lmdb')
         train_chains_path = os.path.join(root, 'lmdb', 'train_chains.pkl')
@@ -334,11 +435,11 @@ class UseOriginalTestSplitManager(BaseSplitManager):
     
 
 if __name__ == "__main__":
-    root = os.path.join(os.getcwd(), 'hotels50k')
+    root = os.environ.get('HOTELS50K_ROOT') or os.path.join(os.getcwd(), 'hotels50k')
     dataset = Hotels50kDataset(root=root, target='hotels', download=True)
 
     print('Retrieving example')
     for obs in dataset:
-        print(obs['label'])
-        print(np.array(obs['data']).shape)
+        print('Label:', obs['label'])
+        print('Data shape:', np.array(obs['data']).shape)
         break
