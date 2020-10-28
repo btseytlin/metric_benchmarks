efc2a5ae16ab884a1314e49000e16c3f7f9e5a94
Boris <b.tseytlin@lambda-it.ru>
2020-10-28 11:39:01 +0300

    Add repr

---
+++ benchmarks_hotels50k/batch_regularizer_reproduce.sh
@@ -0,0 +1,13 @@
+python3 runners/batch_regularizer.py --experiment_name hotels_batch_regularizer_reproduce \
+--dataset~OVERRIDE~ {Hotels50kDataset: {download: False, target: 'chains', root: $PWD/hotels50k}} \
+--trainer~APPLY~2 {dataloader_num_workers: 6, iterations_per_epoch: 500} \
+--tester~APPLY~2 {dataloader_num_workers: 6} \
+--bayes_opt_iters 0 \
+--split_manager~SWAP~1 {IndexSplitManager: {}} \
+--split_manager~APPLY~2 {helper_split_manager: {UseOriginalTestSplitManager: {}}} \
+--loss_funcs~OVERRIDE~ \
+{metric_loss: {ContrastiveLossRegularized: {\
+reg_own_weight: 0.197 reg_own_threshold: 0.066, \
+reg_other_weight: 0.772, reg_other_threshold: 0.436, \
+pos_margin: 0.23, \
+neg_margin: 0.917}}} 
